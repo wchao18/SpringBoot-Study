92f9da034f1d145779ebb262df0c1b1aac588414
wangchao <wangchao@weherepost.com>
2019-08-28 19:04:42 +0800

    增加动态修改日志配置服务

---
+++ log_demo/src/main/java/com/it/controller/LogController.java
@@ -0,0 +1,32 @@
+package com.it.controller;
+
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangchao
+ * @description TODO
+ * @date 2019/08/28 17:25
+ */
+@RestController
+public class LogController {
+
+    private static Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    /**
+     * 查看所有的日志级别：http://ip:port/loggers
+     * 修改日志级别：post: /loggers/com.it.controller  参数：{"configuredLevel":"ERROR"}
+     * 查看日志级别：get:  /loggers/com.it.controller
+     */
+    @RequestMapping("/log")
+    public String log() {
+       logger.info("*******info");
+       logger.error("******error");
+       return "success";
+    }
+
+}
