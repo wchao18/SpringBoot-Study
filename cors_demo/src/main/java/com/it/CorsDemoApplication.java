d6a64dbeaead8e73ec4ce937ff32eb754df5e546
wangchao <wangchao@weherepost.com>
2019-09-09 13:15:03 +0800

    commit

---
+++ cors_demo/src/main/java/com/it/CorsDemoApplication.java
@@ -0,0 +1,17 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsDemoApplication {
+
+    /**
+     * Cross-Origin Resource Sharing
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorsDemoApplication.class, args);
+    }
+
+}
---
+++ https_demo/src/main/java/com/it/HttpsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpsDemoApplication.class, args);
+    }
+
+}
