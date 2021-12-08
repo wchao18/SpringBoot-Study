14f4400365667270955b687d8b88de5da9ba12fb
wangchao <wangchao@weherepost.com>
2021-12-08 13:52:26 +0800

    git ignore

--- bin_log_demo/src/main/java/com/it/config/BinlogConfig.java
+++ bin_log_demo/src/main/java/com/it/config/BinlogConfig.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "adconf.mysql")
+@ConfigurationProperties(prefix = "conf.mysql")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
